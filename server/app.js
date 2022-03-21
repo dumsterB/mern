6cb9da6b4b0c71e3cf06ab7332dcdaefa096c20a
Bexruz <74446521+dumsterB@users.noreply.github.com>
2022-03-21 16:56:44 +0500

    deploy front

--- server/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const placesRoutes = require('./routes/places-routes.js')
-const app = express()
-
-app.use(placesRoutes)
-
-app.listen(3000)
--- server/routes/places-routes.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const router=express.Router()
-
-router.get('/',(req,res,next)=>{
-    console.log('get req places')
-    res.json({message:'it works'})
-})
-module.exports=router
